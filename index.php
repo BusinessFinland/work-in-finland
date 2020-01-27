ab282e3f7ce9791321277f06cea34d4ec9f8c0b3
Jari Pelto-Piri <jari@peltopiri.com>
2020-01-27 14:32:16 +0200

    Update plugin version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Description:     Fetch Job Openings from jobs.workinfinland.fi
  * Text Domain:     jwif
  * Domain Path:     /languages
- * Version:         0.1.5
+ * Version:         0.1.6
  *
  * @package         Jobs_Workinfinland
  */
